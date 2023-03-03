005e22ba72c18de0949d2a083431a398eef86deb
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-03 18:15:42 -0300

    feat: add sidebar component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 import { Header } from './components/Header';
-import { Post } from './Post'
+import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import "./global.css";
 
@@ -11,14 +14,20 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Luan Oliveira" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem non eum sequi! Esse, ex ab animi, eligendi qui asperiores cumque corporis illo vero hic eius reiciendis harum eos alias dolorem!"
-      />
-      <Post 
-        author="Estefâne Oliveira" 
-        content="A different post"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Luan Oliveira" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem non eum sequi! Esse, ex ab animi, eligendi qui asperiores cumque corporis illo vero hic eius reiciendis harum eos alias dolorem!"
+          />
+          <Post 
+            author="Estefâne Oliveira" 
+            content="A different post"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1591267990532-e5bdb1b0ceb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/luanoliveira98.png" />
+
+        <strong>Luan Oliveira</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Edit your profile
+        </a>
+      </footer>
+    </aside>
+  );
+}

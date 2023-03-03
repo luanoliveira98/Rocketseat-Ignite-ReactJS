921e74036a092684810c28072975f973b841a874
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-03 17:34:15 -0300

    feat: add global css

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { Header } from './components/Header';
 import { Post } from './Post'
 
-import "./styles.css";
+import "./global.css";
 
 export function App() {
   const [count, setCount] = useState(0)

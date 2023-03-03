1264417e0d43847b83f65d7cb739da385acf6323
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-03 17:24:41 -0300

    feat: add header component with css module

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+
+import { Header } from './components/Header';
 import { Post } from './Post'
+
+import "./styles.css";
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <Header />
       <Post 
         author="Luan Oliveira" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem non eum sequi! Esse, ex ab animi, eligendi qui asperiores cumque corporis illo vero hic eius reiciendis harum eos alias dolorem!"

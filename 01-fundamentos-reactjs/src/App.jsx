069bb2eb0df981179008e8a8b60a3aaaa95f7c80
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-06 19:28:21 -0300

    feat: add props to post component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -8,6 +8,37 @@ import styles from './App.module.css';
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/luanoliveira98.png",
+      name: "Luan Oliveira",
+      role: "Web developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋"},
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      { type: "link", content: "jane.design/doctorcare"}
+    ],
+    publishedAt: new Date('2023-03-06 19:05:10')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋"},
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      { type: "link", content: "jane.design/doctorcare"}
+    ],
+    publishedAt: new Date('2023-03-01 10:15:10')
+  }
+]
+
 export function App() {
   const [count, setCount] = useState(0)
 
@@ -18,10 +49,19 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          { posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          }) }
         </main>
       </div>
     </div>
   )
 }
+
+

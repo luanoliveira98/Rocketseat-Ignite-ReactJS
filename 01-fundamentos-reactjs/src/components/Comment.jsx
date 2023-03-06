92ced0ace23616ad83e220d9edbc184cab41200c
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-06 18:24:59 -0300

    feat: add comment component

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/luanoliveira98.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Luan Oliveira</strong>
+              <time title="6 de março às 16:50h" dateTime="2023-03-06 16:50:45">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir
+            <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}

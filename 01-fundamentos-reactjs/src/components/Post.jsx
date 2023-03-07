d02b9d4786341fbeba31aa244ff04d36b50dc988
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-07 19:07:55 -0300

    feat: add keys into lists

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -51,10 +51,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         { content.map(line => {
           if (line.type === "paragraph") {
-            return <p>{ line.content }</p>
+            return <p key={line.content}>{ line.content }</p>
           }
           if (line.type === "link") {
-            return <p><a>{ line.content }</a></p>
+            return <p key={line.content}><a>{ line.content }</a></p>
           }
         }) }
       </div>
@@ -76,7 +76,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         { comments.map(comment => {
-          return <Comment content={comment}/>
+          return <Comment key={comment} content={comment}/>
         })}
       </div>
     </article>

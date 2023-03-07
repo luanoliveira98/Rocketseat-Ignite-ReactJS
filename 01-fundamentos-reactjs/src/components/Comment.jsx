c91f3ad04ec6c20bf5870782e0b572a41d9ece3b
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-07 19:19:50 -0300

    feat: add method to delete comments

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/luanoliveira98.png" />
@@ -15,7 +20,7 @@ export function Comment({ content }) {
               <time title="6 de março às 16:50h" dateTime="2023-03-06 16:50:45">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>

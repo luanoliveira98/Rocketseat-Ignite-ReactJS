0f9b0ac8becc04cb5905e4738952b5934713728f
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-08 19:27:14 -0300

    feat: create new version using typescript

---
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import "./global.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/luanoliveira98.png",
+      name: "Luan Oliveira",
+      role: "Web developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋"},
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      { type: "link", content: "jane.design/doctorcare"}
+    ],
+    publishedAt: new Date('2023-03-06 19:05:10')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋"},
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      { type: "link", content: "jane.design/doctorcare"}
+    ],
+    publishedAt: new Date('2023-03-01 10:15:10')
+  }
+]
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          { posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          }) }
+        </main>
+      </div>
+    </div>
+  )
+}
+
+
---
+++ 01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+  const [likeCount, setLikeCount] = useState(0);
+  
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/luanoliveira98.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Luan Oliveira</strong>
+              <time title="6 de março às 16:50h" dateTime="2023-03-06 16:50:45">Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir
+            <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}

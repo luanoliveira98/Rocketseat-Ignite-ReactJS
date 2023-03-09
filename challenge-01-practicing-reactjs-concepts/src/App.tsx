9ee7e9539ea1f4009e4e52cdd3e5d89f557e21d4
Luan Oliveira <luanoliveira17@hotmail.com>
2023-03-09 18:56:37 -0300

    feat: start challenge 01

---
+++ challenge-01-practicing-reactjs-concepts/src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from './components/Header';
+
+import styles from './App.module.css';
+import './global.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
